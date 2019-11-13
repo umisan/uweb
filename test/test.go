06870a63d9a6ab629cdfa56f014b60c2d3014ff6
umisan <t.umisan6@gmail.com>
2019-11-14 00:14:43 +0900

    errofgとfatalfgの仕様変更

--- test/test.go
+++ test/test.go
@@ -2,16 +2,19 @@ package test
 
 import (
 	"testing"
+	"fmt"
 )
 
 //Errorfg this function executes Errorf function with the function name in which it is called and error info
-func Errorfg(t *testing.T, info string) {
+func Errorfg(t *testing.T, info string, a ...interface{}) {
 	funcName := t.Name()
-	t.Errorf("%s: %s", funcName, info)
+	s := fmt.Sprintf(info, a...)
+	t.Errorf("%s: %s", funcName, s)
 }
 
 //Fatalfg this function executes Fatalf function with the function name in which it is called and error info
-func Fatalfg(t *testing.T, info string) {
+func Fatalfg(t *testing.T, info string, a ...interface{}) {
 	funcName := t.Name()
-	t.Fatalf("%s: %s", funcName, info)
+	s := fmt.Sprintf(info, a...)
+	t.Fatalf("%s: %s", funcName, s)
 }
