ea949a15b2b1034ba98667e31114828d65e83013
umisan <t.umisan6@gmail.com>
2020-02-29 13:38:50 +0900

    cors対策の関数を追加

---
+++ view/view.go
@@ -0,0 +1,12 @@
+package view
+
+import (
+	"net/http"
+)
+
+func SetCORS(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8000")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE")
+}
